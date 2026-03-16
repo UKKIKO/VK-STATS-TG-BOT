540b2cac6fcdec9f2226b81195402f6ae9eb583d
QQshka <104214679+UKKIKO@users.noreply.github.com>
2026-03-16 21:02:12 +0700

    Отказ от деплоя проекта, откат до локального запуска

--- Develop/main.py
+++ Develop/main.py
@@ -1,26 +1,14 @@
-import os
 import threading
 import logging
-import telebot
-from flask import Flask, request
-from dotenv import load_dotenv
-from Bot_Dev import bot, logger
+from Bot_Dev import bot
 from Scheduler import main_loop
-from config import TG_ACCESS_TOKEN
 
-load_dotenv()
-app = Flask(__name__)
 
-@app.route(f'/{TG_ACCESS_TOKEN}', methods=['POST'])
-def webhook():
-    if request.headers.get('content-type') == 'application/json':
-        json_string = request.get_data().decode('utf-8')
-        update = telebot.types.Update.de_json(json_string)
-        bot.process_new_updates([update])
-
-        return {'status': 'ok'}, 200
-    else:
-        return {'status': 'bad request'}, 400
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
 
 def run_scheduler_thread():
     logger.info("Запуск планировщика в фоновом потоке...")
@@ -34,19 +22,11 @@ if __name__ == "__main__":
     scheduler_thread.daemon = True
     scheduler_thread.start()
 
-    webhook_url = os.environ.get('WEBHOOK_URL')
+    logger.info(f"Бот запущен!")
+    logger.info("Нажми Ctrl+C, чтобы остановить бота и планировщик.")
 
-    if webhook_url:
-        full_webhook_url = f"{webhook_url}/{TG_ACCESS_TOKEN}"
-
-        logger.info(f"Устанавливаем Webhook на: {full_webhook_url}")
-
-        bot.remove_webhook()
-        bot.set_webhook(url=full_webhook_url)
-    else:
-        logger.warning("Переменная WEBHOOK_URL не найдена. Webhook не настроен.")
-
-    port = int(os.environ.get('PORT', 5000))
-    logger.info(f"Сервер запускается на порту {port}...")
-    app.run(host='0.0.0.0', port=port)
+    try:
+        bot.infinity_polling(timeout=10, long_polling_timeout=5)
+    except KeyboardInterrupt:
+        logger.info("Остановка работы бота...")
 
